2db40b19b0841d36adc928364712c742e5cef1c5
Dmitry K <dmitryk@siklu.com>
2023-12-05 18:05:13 +0200

    move ds_install in separate cmake module

---
+++ cmake/DebugSymbols.cmake
@@ -0,0 +1,12 @@
+set(SYMBOLS_BINARY_PATH ${CMAKE_BINARY_DIR}/symbols)
+file(MAKE_DIRECTORY ${SYMBOLS_BINARY_PATH})
+function (create_ds target)
+    set (TARGET_DS ${SYMBOLS_BINARY_PATH}/${target}.${DEBUG_SYMBOL_SUFFIX})
+
+    add_custom_command(TARGET ${TARGET} POST_BUILD
+            COMMAND ${CMAKE_OBJCOPY} --only-keep-debug $<TARGET_FILE:${target}> ${TARGET_DS}
+            COMMAND ${CMAKE_STRIP} $<TARGET_FILE:${target}>
+            COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink=${TARGET_DS} $<TARGET_FILE:${target}>
+    )
+endfunction()
+install(DIRECTORY ${SYMBOLS_BINARY_PATH} TYPE INFO)
